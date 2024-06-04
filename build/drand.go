27838042cec13a0ff7d3c507eb96321240f812c3
Luis Enrique Fernandez <131024332+antithesis-luis@users.noreply.github.com>
2024-06-04 09:15:35 -0400

    a possible implementation to enable local drand

--- build/drand.go
+++ build/drand.go
@@ -2,6 +2,7 @@ package build
 
 import (
 	"sort"
+	"os"
 
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 )
@@ -30,22 +31,23 @@ const (
 	DrandQuicknet
 )
 
+func getDrandChainInfo() string {
+	b, _ := os.ReadFile(os.Getenv("DRAND_CHAIN_INFO"))
+	str := string(b)
+	return str
+}
+
 var DrandConfigs = map[DrandEnum]dtypes.DrandConfig{
 	DrandMainnet: {
 		Servers: []string{
-			"https://api.drand.sh",
-			"https://api2.drand.sh",
-			"https://api3.drand.sh",
-			"https://drand.cloudflare.com",
-			"https://api.drand.secureweb3.com:6875", // Storswift
+			"http://drand-1",
+			"http://drand-2",
+			"http://drand-3",
 		},
 		Relays: []string{
-			"/dnsaddr/api.drand.sh/",
-			"/dnsaddr/api2.drand.sh/",
-			"/dnsaddr/api3.drand.sh/",
 		},
 		IsChained:     true,
-		ChainInfoJSON: `{"public_key":"868f005eb8e6e4ca0a47c8a77ceaa5309a47978a7c71bc5cce96366b5d7a569937c529eeda66c7293784a9402801af31","period":30,"genesis_time":1595431050,"hash":"8990e7a9aaed2ffed73dbd7092123d6f289930540d7651336225dc172e51b2ce","groupHash":"176f93498eac9ca337150b46d21dd58673ea4e3581185f869672e59fa4cb390a"}`,
+		ChainInfoJSON: getDrandChainInfo(),
 	},
 	DrandQuicknet: {
 		Servers: []string{
